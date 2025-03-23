8db8d3445f9b8e26ba18f8fa17b7fda81d34f093
milrilowe <miles@creovia.io>
2025-03-22 22:30:37 -0700

    netlify build config

--- app.config.ts
+++ app.config.ts
@@ -9,6 +9,9 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
+    server: {
+        preset: 'netlify',
+    },
     vite: {
         plugins: [
             tsConfigPaths({
