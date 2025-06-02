82ac28bb9e008ff5743c2092f7e1006d3321a698
milrilowe <miles@creovia.io>
2025-06-02 16:07:13 -0700

    fix tickets?

--- src/pages/tickets.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = ({ redirect }) => {
-    return redirect('https://www.eventbrite.com/e/phoenix-comedy-underground-best-of-phoenix-showcase-tickets-1380218563879', 302);
-};
